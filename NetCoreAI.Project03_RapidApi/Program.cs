566c5968bb6471a7cbb3e3a8330f78eeb9c1583c
nureddincan <nureddincanhd@outlook.com>
2026-02-12 02:45:12 +0300

    Show the data received from the Rapid API on the console

--- NetCoreAI.Project03_RapidApi/Program.cs
+++ NetCoreAI.Project03_RapidApi/Program.cs
@@ -1,2 +1,31 @@
-﻿// Rapid Api aklınıza gelebilecek bütün konularda api sağlayan çok büyük bir teknoloji
-Console.WriteLine("Hello, World!");
+﻿
+
+using NetCoreAI.Project03_RapidApi.ViewModels;
+using Newtonsoft.Json;
+
+var client = new HttpClient();
+
+List<ApiSeriesViewModel> apiSeriesViewModels = new List<ApiSeriesViewModel>();
+
+var request = new HttpRequestMessage
+{
+    Method = HttpMethod.Get,
+    RequestUri = new Uri("https://imdb-top-100-movies.p.rapidapi.com/series/"),
+    Headers =
+    {
+        { "x-rapidapi-key", "2cd163a4d1mshadfc684ea01fde1p1192b3jsn7f0a16cc3b4d" },
+        { "x-rapidapi-host", "imdb-top-100-movies.p.rapidapi.com" },
+    },
+};
+using (var response = await client.SendAsync(request))
+{
+    response.EnsureSuccessStatusCode();
+    var body = await response.Content.ReadAsStringAsync();
+    // Json gelen veriyi, metne dönüştürme
+    apiSeriesViewModels = JsonConvert.DeserializeObject<List<ApiSeriesViewModel>>(body);
+    foreach (var series in apiSeriesViewModels)
+    {
+        Console.WriteLine(series.rank + " - " + series.title + " - Rating:" + series.rating + " Year:" + series.rating);
+    }
+}
+
