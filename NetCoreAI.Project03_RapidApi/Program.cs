2d923bb011d01012bfa89bf32a5b3ca932e8d62a
nureddincan <nureddincanhd@outlook.com>
2026-02-12 02:24:02 +0300

    Create View Model for Api Series

--- NetCoreAI.Project03_RapidApi/Program.cs
+++ NetCoreAI.Project03_RapidApi/Program.cs
@@ -1,1 +1,2 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// Rapid Api aklınıza gelebilecek bütün konularda api sağlayan çok büyük bir teknoloji
+Console.WriteLine("Hello, World!");
---
+++ NetCoreAI.Project03_RapidApi/ViewModels/ApiSeriesViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace NetCoreAI.Project03_RapidApi.ViewModels
+{
+    public class ApiSeriesViewModel
+    {
+        public int rank { get; set; }
+        public string title { get; set; }
+        public float rating { get; set; }
+        public string year { get; set; }
+    }
+}
