3e0c2560464b1617efa0294e2313e3e287ddf9e0
nureddincan <nureddincanhd@outlook.com>
2026-02-12 01:49:10 +0300

    Add Customer Updation Feature

--- NetCoreAI.Project02_ApiConsumeUI/Controllers/CustomerController.cs
+++ NetCoreAI.Project02_ApiConsumeUI/Controllers/CustomerController.cs
@@ -71,5 +71,36 @@ namespace NetCoreAI.Project02_ApiConsumeUI.Controllers
             }
             return View();
         }
+
+        [HttpGet]
+        public async Task<IActionResult> UpdateCustomer(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"https://localhost:7258/api/Customers/GetCustomer?id={id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                // Serialize normal gelen veriyi json formatına dönüştürür.
+                var values = JsonConvert.DeserializeObject<GetByIdCustomerDto>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateCustomer(UpdateCustomerDto updateCustomerDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            // metni jsona dönüştürüyoruz.
+            var jsonData = JsonConvert.SerializeObject(updateCustomerDto);
+            // Gönderilecek data, data için kullanılacak karakter dizini, gönderilecek datanın formatı
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PutAsync("https://localhost:7258/api/Customers", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("CustomerList");
+            }
+            return View();
+        }
     }
 }
