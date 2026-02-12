5d32bf379769fa9caf5d29c74cc2e08d5810cb9e
nureddincan <nureddincanhd@outlook.com>
2026-02-12 19:57:17 +0300

    Finish OCR Codes

--- NetCoreAI.Project07_TeseractOcr/Program.cs
+++ NetCoreAI.Project07_TeseractOcr/Program.cs
@@ -1,8 +1,33 @@
 ﻿
+using Tesseract;
+
 class Program
 {
-    public static async Task Main(string[] args)
+    static void Main(string[] args)
     {
-
+        Console.WriteLine("Karakter Okuması Yapılacak Resim Yolu: ");
+        string imagePath = Console.ReadLine();
+        Console.WriteLine();
+        // Dil veritabanı yolunu belirtme
+        string tessDataPath = @"C:\tessdata";
+        try
+        {
+            using (var engine = new TesseractEngine(tessDataPath, "eng", EngineMode.Default))
+            {
+                using (var img = Pix.LoadFromFile(imagePath))
+                {
+                    using (var page = engine.Process(img))
+                    {
+                        string text = page.GetText();
+                        Console.WriteLine("Resimden Okunan Metin...");
+                        Console.WriteLine(text);
+                    }
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Bir hata oluştu: {ex.Message}");
+        }
     }
 }
