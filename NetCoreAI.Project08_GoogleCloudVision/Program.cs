5713fa2c27eef5e74fcd6f31f100c27346c0ad89
nureddincan <nureddincanhd@outlook.com>
2026-02-12 22:39:47 +0300

    Finish Google Cloud Vision OCR

---
+++ NetCoreAI.Project08_GoogleCloudVision/Program.cs
@@ -0,0 +1,35 @@
+﻿using Google.Cloud.Vision.V1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Resim Yolunu Giriniz: ");
+        string imagePath = Console.ReadLine();
+        Console.WriteLine();
+        // Buranın içine bir json dosyası alınacak. Bu json dosyası Google API üzerinden bizi karşılayacak
+        string credentialPath = "C:\\Users\\NureddinCan\\Downloads\\visionapiservice-0-865cd886393b.json";
+        Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", credentialPath);
+
+        try
+        {
+            var client = ImageAnnotatorClient.Create();
+
+            var image = Image.FromFile(imagePath);
+            var response = client.DetectText(image);
+            Console.WriteLine("Resimdeki Metin: ");
+            Console.WriteLine();
+            foreach (var annotination in response)
+            {
+                if (!string.IsNullOrEmpty(annotination.Description))
+                {
+                    Console.WriteLine(annotination.Description);
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Bir Hata Oluştu: {ex.Message}");
+        }
+    }
+}
