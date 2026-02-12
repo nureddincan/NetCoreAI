b0771c6b9b5794b2eba793aac73e88a8f4b68fd5
nureddincan <nureddincanhd@outlook.com>
2026-02-12 18:41:57 +0300

    Add audio to text conversion codes

--- NetCoreAI.Project05_OpenAIWhisperAudioToText/Program.cs
+++ NetCoreAI.Project05_OpenAIWhisperAudioToText/Program.cs
@@ -9,7 +9,37 @@ class Program
 
         using (var client = new HttpClient())
         {
-            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer ", apiKey);
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", apiKey);
+
+            // Dosyayla işlem yapmamızı sağlayacak sınıf.
+            var form = new MultipartFormDataContent();
+            // Ses dosyasını yükleme işlemi, byte formatıyla ses dosyasını okuyacağız.
+            var audioContent = new ByteArrayContent(File.ReadAllBytes(audioFilePath));
+            // Ses dosyası için kullanacağımız çıkış formatı
+            audioContent.Headers.ContentType = MediaTypeHeaderValue.Parse("audio/mpeg");
+            // İşleyeceğimiz sesin nerden geleceğini uygulamaya tanıttık.
+            form.Add(audioContent, "file", Path.GetFileName(audioFilePath));
+
+            form.Add(new StringContent("whisper-1"), "model");
+
+            Console.WriteLine("Ses Dosyası İşleniyor, Lütfen Bekleyiniz...");
+
+            // hafızaya alıp okuttuğumuz dosya için OpenAI API'ye post isteği göndereceğiz.
+            // form ses dosyası için gönderdiğimiz content
+            var response = await client.PostAsync("https://api.openai.com/v1/audio/transcriptions", form);
+            if (response.IsSuccessStatusCode)
+            {
+                var result = await response.Content.ReadAsStringAsync();
+                Console.WriteLine("Transkript: ");
+                Console.WriteLine(result);
+            }
+            else
+            {
+                Console.WriteLine($"Hata: {response.StatusCode}");
+                Console.WriteLine(await response.Content.ReadAsStringAsync());
+            }
+
         }
+
     }
 }
