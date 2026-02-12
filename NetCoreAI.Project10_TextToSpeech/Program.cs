f00cccdc723857f5cfa5e1eb4feca3beb423b865
nureddincan <nureddincanhd@outlook.com>
2026-02-13 00:33:32 +0300

    Add Microsoft .NET Text To Speech(System.Speech) Feature

---
+++ NetCoreAI.Project10_TextToSpeech/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Speech.Synthesis;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        SpeechSynthesizer speechSynthesizer = new SpeechSynthesizer();
+
+        speechSynthesizer.Volume = 100;
+        // Normal konuşma hızı
+        speechSynthesizer.Rate = 0;
+
+        Console.Write("Metni Girin: ");
+        string input;
+        input = Console.ReadLine();
+
+        if (!string.IsNullOrEmpty(input))
+        {
+            speechSynthesizer.Speak(input);
+        }
+        else
+        {
+            Console.WriteLine("Metin seslendirilemedi!");
+        }
+
+        Console.ReadLine();
+
+    }
+}
